e91033015ac40dac83de540c6a6fd117d8fd28d2
mk-43 <mkk043@gmail.com>
2022-05-16 21:14:25 +0900

    leetcode

---
+++ leetcode/tmp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int N;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    cin >> N;
+
+    int v;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> v;
+
+    }
+
+
+
+    return 0;
+}
