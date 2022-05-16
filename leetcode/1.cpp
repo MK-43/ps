66330f97a77f209d9914a32cf996f9cd5b673934
mk-43 <mkk043@gmail.com>
2022-05-17 00:42:36 +0900

    leetcode #1

---
+++ leetcode/1.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    // sort first
+    // nlog(n)
+
+    nums.
+  }
+};
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+#endif
+  cin >> N;
+
+  vector<int> vec = {2, 7, 11, 15};
+
+  Solution sol;
+  sol.twoSum(vec, 9);
+
+  return 0;
+}
